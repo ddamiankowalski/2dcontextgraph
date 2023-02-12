345f8067538c626323a15e1b1d2a51d5862d8838
Damian <dkowalski.1997@gmail.com>
2023-02-12 15:37:22 +0100

    added text

--- src/chart/chart-renderer.ts
+++ src/chart/chart-renderer.ts
@@ -10,7 +10,7 @@ export class ChartRenderer implements Renderer {
         this.canvas.width = canvas.offsetWidth;
         this.graphWidth = canvas.width;
         this.graphHeight = canvas.height;
-        this.graphZoom = 100;
+        this.graphZoom = 75;
 
         this.canvas.style.backgroundColor = "#252525";
         this.addCanvasListeners();
@@ -23,12 +23,15 @@ export class ChartRenderer implements Renderer {
     private graphZoom: number;
 
     private horizontalMargin: number = 50;
-    private verticalMargin: number = 20;
+    private verticalMargin: number = 50;
 
     private mouseXPosition: number;
     private mouseYPosition: number;
 
     private xGridThreshold: number = 50;
+
+    private startTime: number = new Date().getTime();
+    private endTime: number = new Date().getTime();
 
     public draw(timePassed: number): void {
         this.context.clearRect(0, 0, this.graphWidth, this.graphHeight);
@@ -40,12 +43,20 @@ export class ChartRenderer implements Renderer {
     drawGrid(): void {
         this.drawVerticalLines();
         //this.drawMousePosition();
+        this.drawTimeline();
     }
 
     private drawVerticalLines(): void {
-        for(let drawingXPosition = 0; drawingXPosition < this.graphWidth; drawingXPosition = drawingXPosition + this.graphZoom) {
-            this.drawLine(drawingXPosition, 0, drawingXPosition, this.graphHeight);
+        for(let drawingXPosition = this.graphWidth; drawingXPosition > 0; drawingXPosition = drawingXPosition - this.graphZoom) {
+            this.drawLine(drawingXPosition, 0, drawingXPosition, this.graphHeight - this.verticalMargin);
         }
+    }
+
+    private drawTimeline(): void {
+        this.context.font = "12px sans-serif";
+        this.context.fillStyle = '#A9A9A9';
+        this.context.fillText(this.startTime.toString(), this.graphWidth - 100, this.graphHeight - 20);
+        this.context.fillText('Current time span between columns:', this.graphWidth / 2 - 100, this.graphHeight - 20);
     }
 
     private drawMousePosition(): void {
@@ -57,28 +68,31 @@ export class ChartRenderer implements Renderer {
         this.context.beginPath();
         this.context.moveTo(xStart, yStart);
         this.context.lineTo(xEnd, yEnd);
-        this.context.strokeStyle = '#FFFFFF';
+        this.context.strokeStyle = '#A9A9A9';
         this.context.lineWidth = 1;
         this.context.stroke();
     }
 
     private addCanvasListeners(): void {
+
+        // wheel event
         this.canvas.addEventListener('wheel', (event: WheelEvent) => {
             if(event.deltaY > 0) {
                 if(this.graphZoom - 2 !== 30) {
-                    this.graphZoom = this.graphZoom - .5;
+                    this.graphZoom = this.graphZoom - 1;
                 }
             } else {
-                this.graphZoom = this.graphZoom + .5;
+                this.graphZoom = this.graphZoom + 1;
             }
 
-            console.log(this.graphZoom, this.xGridThreshold)
-
-            if(this.graphZoom < this.xGridThreshold) {
-                this.graphZoom = this.xGridThreshold + 100;
+            if(this.graphZoom === this.xGridThreshold) {
+                this.graphZoom = this.xGridThreshold * 2 - 1;
+            } else if(this.graphZoom === this.xGridThreshold * 2) {
+                this.graphZoom = this.xGridThreshold + 1;
             }
         })
 
+        // mousemove event
         this.canvas.addEventListener('mousemove', (event: MouseEvent) => {
             this.mouseXPosition = event.clientX;
             this.mouseYPosition = event.clientY;
