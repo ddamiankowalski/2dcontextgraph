9d62ba4acb9e02b130b14927e5bd4bdd91fce0e6
Damian <dkowalski.1997@gmail.com>
2023-02-18 17:16:38 +0100

    changed height

--- src/chart/canvas-dimensions.ts
+++ src/chart/canvas-dimensions.ts
@@ -28,7 +28,7 @@ export class CanvasDimensions {
         this.dimensions.width = this.canvas.offsetWidth;
     }
 
-    private setCanvasStyleWidthAndHeight(width: number = 1380, height: number = 750): void {
+    private setCanvasStyleWidthAndHeight(width: number = 1380, height: number = 550): void {
         this.canvas.style.width = `${width}px`;
         this.canvas.style.height = `${height}px`;
     }
