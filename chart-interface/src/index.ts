7ee5f8f960ba0764493a85267f8c23cc53eb30b6
Damian <dkowalski.1997@gmail.com>
2023-03-06 19:44:16 +0100

    updated tests

---
+++ chart-interface/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/chart-interface/chart-interface.component';
--- spec/helpers/ts-console.js
+++
@@ -1,3 +0,0 @@
-var TSConsoleReporter = require("jasmine-console-reporter");
-jasmine.getEnv().clearReporters();
-jasmine.getEnv().addReporter(new TSConsoleReporter());
