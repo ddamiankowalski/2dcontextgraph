f62d80b5d167b74255fed2fc958ab74b8272c612
Damian <dkowalski.1997@gmail.com>
2023-03-09 20:41:47 +0100

    library publishing changes

--- ng-charts-core/webpack.config.js
+++ ng-charts-core/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: './core/index.ts',
   devtool: 'inline-source-map',
   mode: 'development',
   module: {
