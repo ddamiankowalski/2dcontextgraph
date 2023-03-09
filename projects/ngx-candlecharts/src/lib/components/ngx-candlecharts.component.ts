666f05d203acf81cfd7a0d3f386fb58cd980cfaa
Damian <dkowalski.1997@gmail.com>
2023-03-09 22:37:49 +0100

    moved the files to library folder

--- projects/ngx-candlecharts/src/lib/components/ngx-candlecharts.component.ts
+++ projects/ngx-candlecharts/src/lib/components/ngx-candlecharts.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { Chart } from 'ngx-candlecharts-core/core';
+import { Chart } from 'node_modules/ngx-candlecharts-core/core';
 
 @Component({
   standalone: true,
