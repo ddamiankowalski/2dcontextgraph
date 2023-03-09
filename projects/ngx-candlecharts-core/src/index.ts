41f11db4a7b1338806e8b707145986eb902ca6ef
Damian <dkowalski.1997@gmail.com>
2023-03-09 21:40:13 +0100

    rework of existing repository

--- projects/ngx-candlecharts-core/src/index.ts
+++ projects/ngx-candlecharts-core/src/index.ts
@@ -1,7 +1,2 @@
-import { Chart } from './chart/chart';
-
 export * from './chart/chart';
 export * from './chart/api/api-controller';
-
-const chartElement = document.getElementById('chart');
-const chart = new Chart(chartElement as HTMLCanvasElement);
